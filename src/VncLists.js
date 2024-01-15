48e77b06eab1d2c3f4be0932f21bf4617d3be6b1
faja-ab <fajarfanz378@gmail.com>
2024-01-15 17:58:33 +0000

    ubah link sandbox party 1

--- src/VncLists.js
+++ src/VncLists.js
@@ -1,15 +1,15 @@
 export const vncList = [
-    { vncLink: 'https://codesandbox.io/p/sandbox/upbeat-grass-hfhgcf' },
-    // { vncLink: '' },
-    // { vncLink: '' },
-    // { vncLink: '' },
-    // { vncLink: '' },
-    // { vncLink: '' },
-    // { vncLink: '' },
-    // { vncLink: '' },
-    // { vncLink: '' },
-    // { vncLink: '' },
-    // { vncLink: '' },
-    // { vncLink: '' },
+    { vncLink: 'https://codesandbox.io/p/devbox/dazzling-greider-httjyf'},
+    { vncLink: 'https://codesandbox.io/p/devbox/determined-greider-qz6x5v'},
+    { vncLink: 'https://codesandbox.io/p/devbox/tender-sun-xwtlmc'},
+    { vncLink: 'https://codesandbox.io/p/devbox/nostalgic-sea-nsq7rx'},
+    { vncLink: 'https://codesandbox.io/p/devbox/inspiring-framework-65x39n'},
+    { vncLink: 'https://codesandbox.io/p/devbox/elastic-hooks-83s65c'},
+    { vncLink: 'https://codesandbox.io/p/devbox/affectionate-ishizaka-hx9jt3'},
+    { vncLink: 'https://codesandbox.io/p/devbox/crazy-feather-3lvvvt'},
+    { vncLink: 'https://codesandbox.io/p/devbox/admiring-bash-g8vmck'},
+    { vncLink: 'https://codesandbox.io/p/devbox/aged-platform-jzgccg'},
+    { vncLink: 'https://codesandbox.io/p/devbox/wizardly-julien-ygtq3z'},
+    { vncLink: 'https://codesandbox.io/p/devbox/hidden-worker-ndhfwj'}
     // ... (tambahkan item lainnya sesuai kebutuhan)
 ];

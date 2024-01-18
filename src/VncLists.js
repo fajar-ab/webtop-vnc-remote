a924169b7efdc6d31f3ffc90aebbb93aecfd58eb
faja-ab <fajarfanz378@gmail.com>
2024-01-18 09:53:00 +0000

    perbaiki link vnc

--- src/VncLists.js
+++ src/VncLists.js
@@ -1,14 +1,15 @@
 export const vncList = [
+    { vncLink: 'https://httjyf-3000.csb.app'},
+    { vncLink: 'https://qz6x5v-3000.csb.app'},
+    { vncLink: 'https://xwtlmc-3000.csb.app'},
+    { vncLink: 'https://nsq7rx-3000.csb.app'},
+    { vncLink: 'https://65x39n-3000.csb.app'},
     { vncLink: 'https://83s65c-3000.csb.app'},
     { vncLink: 'https://hx9jt3-3000.csb.app'},
     { vncLink: 'https://3lvvvt-3000.csb.app'},
     { vncLink: 'https://g8vmck-3000.csb.app'},
     { vncLink: 'https://jzgccg-3000.csb.app'},
     { vncLink: 'https://ygtq3z-3000.csb.app'},
-    { vncLink: 'https://ndhfwj-3000.csb.app'},
-    { vncLink: 'https://ppns5k-3000.csb.app'},
-    { vncLink: 'https://lt9nsj-3000.csb.app'},
-    { vncLink: 'https://ksx29c-3000.csb.app'},
-    { vncLink: 'https://697wz7-3000.csb.app'},
+    { vncLink: 'https://ndhfwj-3000.csb.app'}
     // ... (tambahkan item lainnya sesuai kebutuhan)
 ];

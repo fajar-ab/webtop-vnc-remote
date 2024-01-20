11755b7504fb2213ec7d86323299e802120ec206
faja-ab <fajarfanz378@gmail.com>
2024-01-20 12:20:19 +0000

    tambah link sandbox party 2

--- src/VncLists.js
+++ src/VncLists.js
@@ -1,4 +1,15 @@
 export const vncList = [
-    { vncLink: ''},
+    { vncLink: 'https://codesandbox.io/p/devbox/busy-joji-ppns5k'},
+    { vncLink: 'https://codesandbox.io/p/devbox/winter-dew-lt9nsj'},
+    { vncLink: 'https://codesandbox.io/p/devbox/wonderful-maxwell-forked-ksx29c'},
+    { vncLink: 'https://codesandbox.io/p/devbox/wonderful-maxwell-697wz7'},
+    { vncLink: 'https://codesandbox.io/p/sandbox/upbeat-grass-hfhgcf'},
+    { vncLink: 'https://codesandbox.io/p/sandbox/recursing-keller-h8dptw'},
+    { vncLink: 'https://codesandbox.io/p/devbox/dank-feather-y5wtzr'},
+    { vncLink: 'https://codesandbox.io/p/devbox/rough-surf-hdcr9c'},
+    { vncLink: 'https://codesandbox.io/p/devbox/xenodochial-mountain-hhwl4k'},
+    { vncLink: 'https://codesandbox.io/p/devbox/goofy-kepler-fwtkyf'},
+    { vncLink: 'https://codesandbox.io/p/devbox/jolly-hill-knh8vr'},
+    { vncLink: 'https://codesandbox.io/p/devbox/romantic-thunder-6tj45l'},
     // ... (tambahkan item lainnya sesuai kebutuhan)
 ];

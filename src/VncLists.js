199b13f398f1e59129714628c4e6729473c4636d
dila ayunda <157418013+dilaayunda110210@users.noreply.github.com>
2024-01-22 20:56:10 +0000

    ubah code

--- src/VncLists.js
+++ src/VncLists.js
@@ -1,16 +1,15 @@
 export const vncList = [
-    { vncLink: '' },
-    { vncLink: '' },
-    { vncLink: '' },
-    { vncLink: '' },
-    { vncLink: '' },
-    { vncLink: '' },
-    { vncLink: '' },
-    { vncLink: '' },
-    { vncLink: '' },
-    { vncLink: '' },
-    { vncLink: '' },
-    { vncLink: '' },
-    
-    // ... (tambahkan item lainnya sesuai kebutuhan)
+  { vncLink: "https://httjyf-3000.csb.app" },
+  { vncLink: "https://qz6x5v-3000.csb.app" },
+  { vncLink: "https://xwtlmc-3000.csb.app" },
+  { vncLink: "https://nsq7rx-3000.csb.app" },
+  { vncLink: "https://65x39n-3000.csb.app" },
+  { vncLink: "https://83s65c-3000.csb.app" },
+  { vncLink: "https://hx9jt3-3000.csb.app" },
+  { vncLink: "https://3lvvvt-3000.csb.app" },
+  { vncLink: "https://g8vmck-3000.csb.app" },
+  { vncLink: "https://jzgccg-3000.csb.app" },
+  { vncLink: "https://ygtq3z-3000.csb.app" },
+  { vncLink: "https://ndhfwj-3000.csb.app" },
+  // ... (tambahkan item lainnya sesuai kebutuhan)
 ];

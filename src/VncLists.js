9511d95b8c2cb55570624f43bb3f68f5edef6f78
fajar-ab <fajarfanz378@gmail.com>
2026-04-30 22:03:39 +0700

    update link

--- src/VncLists.js
+++ src/VncLists.js
@@ -1,8 +1,8 @@
 export const vncList = [
     { vncLink: 'https://3nydnh-3000.csb.app/' },
-    { vncLink: 'https://zgrpxp-3000.csb.app/' },
+    { vncLink: 'https://shlsxp-3000.csb.app/' },
     { vncLink: 'https://tps3h6-3000.csb.app/' },
-    { vncLink: 'https://2xlqj2-3000.csb.app/' },
+    { vncLink: 'https://svwhsz-3000.csb.app/' },
     { vncLink: '' },
     { vncLink: '' },
     { vncLink: '' },
